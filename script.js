99cf8ea6b06491f6161db7ae4609a9f288a96aed
kolatham <92490383+kolatham@users.noreply.github.com>
2021-12-02 19:48:04 -0500

    added some more to my comments, stuck on where to go next and how to phrase things

--- script.js
+++ script.js
@@ -15,4 +15,22 @@ for (let i = 0; i< textArea.length; i++) {
         $(currentEl).addClass("future")
     }
 }
+// grab all the buttons using class
+var button =document.querySelectorAll(".saveBtn")
+// loop over buttons
+// for (let i =0; i < saveBtn.length; i++) {
+//    var saveBtn = ("button")
+// }
+
+// add eventListener to each button 
+// inside eventListener call localStorage function
+document.addEventListener("click", function saveBtn(){
+    document.getElementById("saveBtn").innerHTML = localStorage.saveBtn
+})
+ 
+// inside localStorage function, loop over text areas
+
+// grab value of textArea var value = textArea[i].value
+
+// localStorage.setItem(textArea[i].id, value)
 console.log(moment().hours())
